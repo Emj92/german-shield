a8b9e63133fb465905ddb9ae4f489cb21feca0cf
Erwin Meindl <kontakt@meindl-webdesign.de>
2025-12-03 15:09:10 +0100

    Update 03.12.2025 15:09:10,52

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,9 @@ git commit -m "Update %date% %time%"
 git push origin main
 
 echo Deploye auf Server...
-ssh -o StrictHostKeyChecking=no -T erwinneu@188.245.101.122 "cd /var/www/germanfence.de/german-shield && bash deploy.sh"
+:: HIER war der Fehler: Der Pfad zeigt jetzt auf die sichere Datei
+:: Das "-t" erlaubt dir, dein Passwort einzugeben, falls sudo danach fragt
+ssh -o StrictHostKeyChecking=no -t erwinneu@188.245.101.122 "sudo bash /var/www/germanfence.de/deploy.sh"
 
 echo Fertig!
 pause
