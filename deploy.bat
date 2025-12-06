c0b2500666204a2dd1f141f767b375c9de35025b
Erwin Meindl <kontakt@meindl-webdesign.de>
2025-12-06 15:15:08 +0100

    Update 06.12.2025 15:15:08,59

--- deploy.bat
+++ deploy.bat
@@ -5,7 +5,8 @@ git commit -m "Update %date% %time%"
 git push origin main
 
 echo Deploye auf Server...
-ssh -i "C:\Users\emein\.ssh\id_ed25519" erwin@188.245.101.122 "cd /var/www/germanfence.de/german-shield && ./deploy-server.sh"
+:: KORREKTUR: ./deploy.sh statt ./deploy-server.sh
+ssh -i "C:\Users\emein\.ssh\id_ed25519" erwin@188.245.101.122 "cd /var/www/germanfence.de/german-shield && ./deploy.sh"
 
 echo Fertig!
 pause
